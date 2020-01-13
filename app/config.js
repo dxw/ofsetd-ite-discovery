21c52017289903de39d2a90f773209351b66683a
Ert <ert@dxw.com>
2020-01-13 08:49:14 +0000

    Change of service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage your inspections',
+  serviceName: 'Manage your Ofsted inspections',
 
   // Default port that prototype runs on
   port: '3000',
